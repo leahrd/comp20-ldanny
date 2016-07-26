668c0e23561f8a8c9bf91e40446ba78489eed09f
Leah <leahdanny1991@yahoo.com>
2016-07-25 20:26:20 -0400

    Cleaning up 2

--- mbta/map.js
+++ mbta/map.js
@@ -208,25 +208,3 @@ google.maps.event.addDomListener(window, 'load', mapping);
 
 
 
-	 /*	 var mapProps =new google.maps.LatLng(42.352271,-71.05524200000001);
-	 var mapPropOpt={
-	     zoom:5,
-	     center:mapProps
-	 };
-	 
-	     //mapTypeId:google.maps.MapTypeId.ROADMAP
-    
-	 var map = new google.maps.Map(document.getElementById("map"),mapPropOpt);
-	 var contentString = 'South Station';
-	 var infowindow = new google.maps.InfoWindow({});
-	 var marker = new google.maps.Marker({
-		 posiion: mapProps,
-		 map: map,
-		 title: 'S Station'
-	     });
-
-       	 google.maps.event.addListener(marker, 'click',function mapping());
-     }
-google.maps.event.addDomListener(window,'load',mapping);
-	 */
-
