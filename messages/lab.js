6712c16be07102b913818fe233ac36f5bf3959ff
Leah <leahdanny1991@yahoo.com>
2016-07-30 09:56:25 -0400

    Parsing JSON

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,27 @@
 // Your JavaScript goes here...
+
+var data = new XMLHttpRequest();
+
+function parse() {
+    data.open("GET", "data.json", true);
+
+
+
+    data.onreadystatechange = function() {
+	if(data.readyState == 4 && data.status == 200){
+	    console.log("Got data");
+	    info = data.responseText;
+	    console.log(info);
+	    text = JSON.parse(info);
+	    document.getElementById("messages").innerHTML = text[0].content + " " + text[0].username + "<br />" + text[1].content + " " + text[1].username;
+	}
+	else if(data.readyState == 4 && data.status !=200) {
+	    document.getElementById("messages").innerHTML = "<p> Something has gone terribly wrong</p>";
+	}
+	else {
+	    console.log("In progress...");
+	}
+    };
+
+    data.send(null);
+}
